66f2a813d8bbb79dc399cec002fb67ee0b5804b3
steve0hh <stevettan@gmail.com>
2017-08-16 18:06:17 +0800

    Add render_png

--- lib/splash_client.ex
+++ lib/splash_client.ex
@@ -3,16 +3,33 @@ defmodule SplashClient do
   Documentation for SplashClient.
   """
 
+  def render_png(url, width \\ nil, height\\ nil, render_all \\ nil, wait \\ nil) do
+    render_png_url(url, width, height, render_all, wait)
+    |> HTTPoison.get!()
+    |> Map.fetch!(:body)
+  end
+
   @doc """
-  Hello world.
+
 
   ## Examples
 
-      iex> SplashClient.hello
-      :world
+      iex> SplashClient.render_png_url("http://www.example.com")
+      "http://0.0.0.0:8050/render.png?&url=http://www.example.com"
 
   """
-  def hello do
-    :world
+  def render_png_url(url, width \\ nil, height\\ nil, render_all \\ nil, wait \\ nil) do
+    "http://0.0.0.0:8050/render.png?"
+    |> build_params("url", url)
+    |> build_params("width", width)
+    |> build_params("height", height)
+    |> build_params("render_all", render_all)
+    |> build_params("wait", wait)
   end
+
+  defp build_params(url, _param, nil), do: url
+  defp build_params(url, param, value) do
+    url <> "&" <> param <> "=" <> value
+  end
+
 end
--- test/splash_client_test.exs
+++ test/splash_client_test.exs
@@ -2,7 +2,7 @@ defmodule SplashClientTest do
   use ExUnit.Case
   doctest SplashClient
 
-  test "greets the world" do
-    assert SplashClient.hello() == :world
+  test "Returns render.png url" do
+    assert SplashClient.render_png_url("http://www.google.com") == "http://0.0.0.0:8050/render.png?&url=http://www.google.com"
   end
 end
